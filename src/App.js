528c36e38b8b9c16ad1f8d6fed156a93da551dfc
Kuldeep singh <singh.kuldeep22798@gmail.com>
2023-09-25 01:38:54 +0530

    enable light and dark mode on navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import AboutUs from './components/AboutUs';
 
 function App() {
+
+
+
+  
   return (
     <>
     <Navbar title="Text-Conversion" ></Navbar>
     <div className="container">
     <TextForm heading='Enter Text to Conversion'></TextForm>
     </div>
+
+
+    <AboutUs/>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 export default function Navbar(props) {
+
+  const [mystyle, setMystyle] = useState({
+    color:'dark',
+    backgroundColor:'white',
+    text:'dark'
+  });
+
+  const switchh = () => {
+    if(mystyle.color=='white'){
+      setMystyle({
+        color:'black',
+        backgroundColor:'white',
+        text:'dark'
+      });
+    }
+    else{
+      setMystyle({
+        color:'white',
+        backgroundColor:'black',
+        text:'light'
+      })
+    }
+}
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a className="navbar-brand" href="/">{props.title}</a>
+    <nav className="navbar navbar-expand-lg"  style={{backgroundColor:mystyle.backgroundColor}}>
+    <a className="navbar-brand" href ="/" style={{color:mystyle.color}}>{props.title}</a>
+
+
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -11,16 +38,14 @@ export default function Navbar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
-          <a className="nav-link" href="/">Home</a>
+          <a className="nav-link" href="/" style={{color:mystyle.color}}>Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
+          <a className="nav-link" href="/" style={{color:mystyle.color}}>About</a>
         </li>
       </ul>
-      <form className="form-inline my-2 my-lg-0">
-        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+      
+    <button type="button" onClick={switchh} className={"btn text-left btn-"+mystyle.text}>Switch to {mystyle.text} </button>
     </div>
   </nav>
   )
